c4472a4a42b9f0fe9fb49d7e8bc19ec9dd54ea04
TamaoSan <thanakitmoss@gmail.com>
2024-03-25 15:33:49 +0700

    move logo to left

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function TopMenu() {
     return (
         <div className="h-[50px] bg-white fixed top-0 left-0 right-0 z-30 border-2 border-neutral-300 border-solid flex flex-row-reverse">
-            <Link href="/">
+            <Link href="/" className="flex flex-row absolute left-0 h-full">
                 <Image src={'/img/logo.jpg'} alt="logo" className="h-full w-auto" width={0} height={0} sizes='100vh'/>
             </Link>
             <TopMenuItem title='Booking' pageRef='/booking'/>
