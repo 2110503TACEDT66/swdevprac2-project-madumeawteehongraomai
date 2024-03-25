f313c45d7a2fe7209e1a94bba4830de6cc5f2879
TaritArtchana <beer.tarit@gmail.com>
2024-03-25 16:26:12 +0700

    prien roob

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function Profile() {
     return (
         <main className="w-[100%] flex flex-col bg-white">
-            <div className="text-4xl font-light text-neutral-400 font-family: 'Roboto' pr-4">YOUR</div>
+            <div className="text-4xl font-light text-neutral-400 font-family: 'Roboto' pr-4 mt-8">YOUR</div>
             <div className="text-4xl font-bold text-black font-family: 'Roboto'  pr-4">PROFILE</div>
             <div className="flex justify-center w-10/12 mx-auto">
-                <div className="border-b border-gray-300 w-full"></div> {/* Small and thin line */}
+                <div className="border-b border-gray-300 w-full"></div> 
             </div>
-            
+
             <div className="flex flex-col justify-center items-center h-screen">
-                <div className="bg-gray-100 p-4 w-10/12 mx-auto mb-4">
+                <div className="bg-gray-100 p-4 w-10/12 mx-auto mb-4 mt-8">
                     <p className="text-gray-700 text-medium">Name</p>
                     <p className="text-gray-700 text-xl">Moss Rudyang</p>
                 </div>
@@ -24,9 +24,13 @@ export default function Profile() {
                     <p className="text-gray-700 text-medium">TEL</p>
                     <p className="text-gray-700 text-xl">091-XXX-XXX</p>
                 </div>
-            
+
+                <div className="bg-gray-100 p-4 w-10/12 mx-auto mb-4">
+                    <p className="text-gray-700 text-medium">ROLE</p>
+                    <p className="text-gray-700 text-xl">USER</p>
+                </div>
+                
             </div>
         </main>
-
     );
 }
