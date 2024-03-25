2000284f90a481b3e49d61e5234377add3922c6d
TaritArtchana <beer.tarit@gmail.com>
2024-03-26 04:03:44 +0700

    enter len

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,13 @@
 import VideoBanner from "./VideoBanner"
+
+
+
 import DescriptionBanner from "./DescriptionBanner"
 
 export default function Banner() {
+    
+    
+    
     return (
         <div className="block mx-[8%] my-10 mt-0 absolute w-[85%] h-[60%] place-content-center bg-[#F4F4F4] flex flex-row">
             <VideoBanner></VideoBanner>
