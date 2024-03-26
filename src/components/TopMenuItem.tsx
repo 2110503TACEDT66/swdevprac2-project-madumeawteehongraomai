dd595488597c7f29e9375d196508742c71beb317
TaritArtchana <beer.tarit@gmail.com>
2024-03-26 12:28:06 +0700

    topmenuuuuuuuuuu

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function TopMenuItem ( { title, pageRef } : { title:string, pageRef:string } ) {
     return (
-        <Link href={pageRef} className='w-32 text-center mt-auto mb-auto font-topitem text-xs text-[#333333]'>
+        <Link href={pageRef} className='w-32 text-center mt-auto mb-auto font-topitem text-xs text-[#333333] hover:text-red-500'>
             {title}
         </Link>
     )
